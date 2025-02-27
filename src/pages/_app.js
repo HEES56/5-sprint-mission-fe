7045629a739685ddab0b763804ae26fbaa0df454
HEES56 <rlagmltjd125@gmail.com>
2025-02-27 10:39:46 +0900

    [sprint] missoin 8

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
+import localFont from "next/font/local";
+import Header from "@/components/shared/Header";
+import { Footer } from "@/components/shared/Footer";
+
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={pretendard.variable}>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,16 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        custom: {
+          blue: "#3692FF",
+          gray: "#F9FAFB",
+        },
       },
     },
   },
